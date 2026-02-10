3e46ce8a6dc3eb2094cf79cba2e171a648da6966
gayannabeth <annabethbruce379@gmail.com>
2026-02-10 18:27:26 -0500

    Added phase ordering for item groups

--- src/main/java/gay/mountainspring/aquifer/Aquifer.java
+++ src/main/java/gay/mountainspring/aquifer/Aquifer.java
@@ -23,10 +23,24 @@ import gay.mountainspring.aquifer.util.ItemUtil;
 import gay.mountainspring.aquifer.util.RepairUtil;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.event.lifecycle.v1.ServerLifecycleEvents;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.fabricmc.loader.api.FabricLoader;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.RegistryKey;
+import net.minecraft.util.Identifier;
 
 public class Aquifer implements ModInitializer {
 	public static final String MOD_ID = "aquifer";
+	
+	public static final Identifier AQUIFER_ID = Identifier.of(MOD_ID, MOD_ID);
+	public static final Identifier CAULDRONSPLUS_ID = Identifier.of(MOD_ID, "cauldronsplus");
+	public static final Identifier CAULDRONSPLUS_SSWPLUS_ID = Identifier.of(MOD_ID, "cauldronsplus_sswplus");
+	public static final Identifier MOUNTAINSPRING_ID = Identifier.of(MOD_ID, "mountainspring");
+	public static final Identifier MS_ID = Identifier.of(MOD_ID, "ms");
+	public static final Identifier NANVIL_ID = Identifier.of(MOD_ID, "nanvil");
+	public static final Identifier SSWPLUS_ID = Identifier.of(MOD_ID, "sswplus");
+	
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 	public static final Gson GSON = new GsonBuilder()
 			.setPrettyPrinting()
@@ -51,6 +65,20 @@ public class Aquifer implements ModInitializer {
 		RepairUtil.init();
 		BlockUtil.init();
 		ItemUtil.init();
+		
+		ItemGroupEvents.MODIFY_ENTRIES_ALL.addPhaseOrdering(SSWPLUS_ID, MOUNTAINSPRING_ID);
+		ItemGroupEvents.MODIFY_ENTRIES_ALL.addPhaseOrdering(CAULDRONSPLUS_ID, MOUNTAINSPRING_ID);
+		ItemGroupEvents.MODIFY_ENTRIES_ALL.addPhaseOrdering(SSWPLUS_ID, CAULDRONSPLUS_SSWPLUS_ID);
+		ItemGroupEvents.MODIFY_ENTRIES_ALL.addPhaseOrdering(CAULDRONSPLUS_ID, CAULDRONSPLUS_SSWPLUS_ID);
+		ItemGroupEvents.MODIFY_ENTRIES_ALL.addPhaseOrdering(CAULDRONSPLUS_SSWPLUS_ID, MOUNTAINSPRING_ID);
+		
+		for (RegistryKey<ItemGroup> key : Registries.ITEM_GROUP.getKeys()) {
+			ItemGroupEvents.modifyEntriesEvent(key).addPhaseOrdering(SSWPLUS_ID, MOUNTAINSPRING_ID);
+			ItemGroupEvents.modifyEntriesEvent(key).addPhaseOrdering(CAULDRONSPLUS_ID, MOUNTAINSPRING_ID);
+			ItemGroupEvents.modifyEntriesEvent(key).addPhaseOrdering(SSWPLUS_ID, CAULDRONSPLUS_SSWPLUS_ID);
+			ItemGroupEvents.modifyEntriesEvent(key).addPhaseOrdering(CAULDRONSPLUS_ID, CAULDRONSPLUS_SSWPLUS_ID);
+			ItemGroupEvents.modifyEntriesEvent(key).addPhaseOrdering(CAULDRONSPLUS_SSWPLUS_ID, MOUNTAINSPRING_ID);
+		}
 		
 		ServerLifecycleEvents.SERVER_STARTING.register(server -> validateCauldronGroups());
 	}
